84cc1e1f53aeaa3b0b0a428bbead4258c9b2938d
kibigo <jacksonkibigo99@gmail.com>
2023-08-22 22:42:55 +0300

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const styleColor = {
+    color: 'firebrick'
+  
+  };
+
+  return <div id="home">
+    <h1 style={styleColor}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
